1469155af811a7e3508376bb0b19fcc2a23a5ef4
Trevor Menagh <trev@trevreport.org>
2011-03-07 21:43:17 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EnvJasmineRails3Example::Application.config.secret_token = '1c78ae248fc707801ba211d2a79de569552135b2643650df692c5fca9af92c9807d32b15ecb5c925796c84cf1646e91ca023feacf41d9dafd197a83d2e00d897'
